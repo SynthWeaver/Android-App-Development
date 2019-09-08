e65fb5372278dac1b2d900b767099143a6cf8478
Tim Terwijn <tim.redbutton@gmail.com>
2019-09-08 16:17:29 +0200

    Finished 1th exercise

--- Level1/Example/app/src/main/java/nl/terwijn/picturequiz/MainActivity.kt
+++ Level1/Example/app/src/main/java/nl/terwijn/picturequiz/MainActivity.kt
@@ -2,11 +2,27 @@ package nl.terwijn.picturequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSubmit.setOnClickListener{
+            checkAnswer();
+        }
+    }
+
+    private fun checkAnswer(){
+        val answer = etInput.text.toString();
+
+        if(answer == getString(R.string.giraffe)){
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show();
+        }
     }
 }
