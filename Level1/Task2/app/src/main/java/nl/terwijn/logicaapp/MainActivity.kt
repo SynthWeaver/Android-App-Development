f9f6e9a17302f053b330c341224d33ecd667c6c5
Tim Terwijn <tim.redbutton@gmail.com>
2019-09-08 20:04:36 +0200

    Task 2 done

--- Level1/Task2/app/src/main/java/nl/terwijn/logicaapp/MainActivity.kt
+++ Level1/Task2/app/src/main/java/nl/terwijn/logicaapp/MainActivity.kt
@@ -2,6 +2,8 @@ package nl.terwijn.logicaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnSubmit.setOnClickListener{
+            onSubmitButtonClick()
+        }
+    }
 
+    private fun onSubmitButtonClick(){
+        val input1 = ivInput1.text.toString().toUpperCase()
+        val input2 = ivInput2.text.toString().toUpperCase()
+        val input3 = ivInput3.text.toString().toUpperCase()
+        val input4 = ivInput4.text.toString().toUpperCase()
+
+        if( input1 == "T" &&
+            input2 == "F" &&
+            input3 == "F" &&
+            input4 == "F") {
+                Toast.makeText(this, getText(R.string.correct),Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, getText(R.string.incorrect),Toast.LENGTH_SHORT).show()
+        }
+
+        ivInput1.setText("")
+        ivInput2.setText("")
+        ivInput3.setText("")
+        ivInput4.setText("")
     }
 }
