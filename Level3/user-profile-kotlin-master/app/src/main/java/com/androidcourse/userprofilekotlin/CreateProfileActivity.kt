a77d7e799cae1b127c3edd11e0ebf5c6f6099762
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-10-04 11:43:21 +0200

    Almost works

--- Level3/user-profile-kotlin-master/app/src/main/java/com/androidcourse/userprofilekotlin/CreateProfileActivity.kt
+++ Level3/user-profile-kotlin-master/app/src/main/java/com/androidcourse/userprofilekotlin/CreateProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.androidcourse.userprofilekotlin
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_create_profile.*
@@ -19,7 +21,25 @@ class CreateProfileActivity : AppCompatActivity() {
         btnOpenGalery.setOnClickListener {
             onGalleryClick()
         }
+        btnConfirm.setOnClickListener {
+            onConfirmClick()
+        }
     }
+
+    private fun onConfirmClick() {
+        val profile = Profile(
+            etFirstName.text.toString(),
+            etLastName.text.toString(),
+            etDescription.text.toString(),
+            profileImageUri
+        )
+        val profileActivityIntent = Intent(this,
+            ProfileActivity::class.java)
+        profileActivityIntent.putExtra(ProfileActivity.PROFILE_EXTRA,
+            profile)
+        startActivity(profileActivityIntent)
+    }
+
 
     private fun onGalleryClick() {
         // Create an Intent with action as ACTION_PICK
@@ -28,5 +48,18 @@ class CreateProfileActivity : AppCompatActivity() {
         galleryIntent.type = "image/*"
         // Start the activity using the gallery intent
         startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    private var profileImageUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                GALLERY_REQUEST_CODE -> {
+                    profileImageUri = data?.data
+                    imgProfilePicture.setImageURI(profileImageUri)
+                }
+            }
+        }
     }
 }
