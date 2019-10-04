0df241ba90ded1470d3021a816561d368a3af0ce
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-10-04 11:11:27 +0200

    create profile

---
+++ Level3/user-profile-kotlin-master/app/src/main/java/com/androidcourse/userprofilekotlin/CreateProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.androidcourse.userprofilekotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+    }
+}
--- Level3/user-profile-kotlin-master/app/src/main/java/com/androidcourse/userprofilekotlin/SplashActivity.kt
+++ Level3/user-profile-kotlin-master/app/src/main/java/com/androidcourse/userprofilekotlin/SplashActivity.kt
@@ -1,9 +1,9 @@
 package com.androidcourse.userprofilekotlin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
