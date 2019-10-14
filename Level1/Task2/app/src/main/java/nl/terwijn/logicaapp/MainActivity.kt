a8237359e6663cb3c820e93d3595595824540e00
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-10-14 15:21:50 +0200

    panic push: limit logica app input

--- Level1/Task2/app/src/main/java/nl/terwijn/logicaapp/MainActivity.kt
+++ Level1/Task2/app/src/main/java/nl/terwijn/logicaapp/MainActivity.kt
@@ -1,8 +1,8 @@
 package nl.terwijn.logicaapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
