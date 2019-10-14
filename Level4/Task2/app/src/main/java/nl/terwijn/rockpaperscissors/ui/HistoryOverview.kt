00c4dff4023259a2d56406c2113d074c9d0e0221
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-10-14 08:35:52 +0200

    Show history works

--- Level4/Task2/app/src/main/java/nl/terwijn/rockpaperscissors/ui/HistoryOverview.kt
+++ Level4/Task2/app/src/main/java/nl/terwijn/rockpaperscissors/ui/HistoryOverview.kt
@@ -1,16 +1,32 @@
 package nl.terwijn.rockpaperscissors.ui
 
+import android.content.Context
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_history_overview.*
+import kotlinx.android.synthetic.main.history.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import nl.terwijn.rockpaperscissors.R
+import nl.terwijn.shoppinglistkotlin.database.ResultRepository
+import nl.terwijn.shoppinglistkotlin.model.ResultData
+
 
 class HistoryOverview : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history_overview)
+
+        resultRepository = ResultRepository(this)
+
+        this.initViews()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -26,6 +42,55 @@ class HistoryOverview : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_delete_history_list -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private val results = arrayListOf<ResultData>()
+    private val resultAdapter = ResultAdapter(results)
+    private lateinit var resultRepository: ResultRepository
+
+    private fun initViews(){
+        rvResults.layoutManager = StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL)
+        rvResults.adapter = this.resultAdapter
+
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val reminders = withContext(Dispatchers.IO) {
+                resultRepository.getAllResults()
+            }
+            this@HistoryOverview.results.clear()
+            this@HistoryOverview.results.addAll(reminders)
+
+            resultAdapter.notifyDataSetChanged()
+        }
+    }
+
+    public class ResultAdapter(private val results: List<ResultData>) :
+        RecyclerView.Adapter<ResultAdapter.ViewHolder>() {
+        lateinit var context: Context
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType:
+        Int): ViewHolder {
+            context = parent.context
+            return ViewHolder(
+                LayoutInflater.from(context).inflate(R.layout.history, parent, false)
+            )
+        }
+
+        override fun getItemCount(): Int {
+            return results.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int)
+        {
+            holder.bind(results[position])
+        }
+
+        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            fun bind(resultData : ResultData) {
+                itemView.tvResult.text = resultData.result
+                itemView.tvDate.text = resultData.date
+            }
         }
     }
 }
