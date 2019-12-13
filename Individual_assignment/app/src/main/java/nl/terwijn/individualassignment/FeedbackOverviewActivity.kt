6315fd184f6e9b9f373019c1cb422c09d733e9f0
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-12-13 09:31:27 +0100

    All screens are done

--- Individual_assignment/app/src/main/java/nl/terwijn/individualassignment/FeedbackOverviewActivity.kt
+++ Individual_assignment/app/src/main/java/nl/terwijn/individualassignment/FeedbackOverviewActivity.kt
@@ -3,10 +3,10 @@ package nl.terwijn.individualassignment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class FeedbackOverviewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_feedback_overview)
     }
 }
