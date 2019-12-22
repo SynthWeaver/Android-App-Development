5005667e9fc776e604b13a9b8fa8b633886c7cbf
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-12-22 12:19:43 +0100

    task 6.2 gritview works again

--- Level6/Task2/app/src/main/java/nl/terwijn/movielist/views/main/MainActivity.kt
+++ Level6/Task2/app/src/main/java/nl/terwijn/movielist/views/main/MainActivity.kt
@@ -1,18 +1,19 @@
 package nl.terwijn.movielist.views.main
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_movie.view.*
+import kotlinx.android.synthetic.main.movie.view.*
 import nl.terwijn.movielist.R
 import nl.terwijn.movielist.models.MainViewModel
 import nl.terwijn.movielist.models.Movie
@@ -21,10 +22,6 @@ import nl.terwijn.movielist.views.movie_details.MovieDetailsActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-
-    private val movies = arrayListOf<Movie>()
-    private val movieAdapter =
-        MovieAdapter(this, movies)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,58 +36,54 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.getPopularMoviesByYear(2018)
 
-//        // Observe the trivia object.
-//        viewModel.trivia.observe(this, Observer {
-//            tvTrivia.text = it?.text
-//        })
-//
-//        // Observe the error message.
-//        viewModel.error.observe(this, Observer {
-//            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
-//        })
+        // Observe the trivia object.
+        viewModel.movies.observe(this, Observer {
+            val adapter = MovieAdapter(this, it)
+            this.gv_movies.adapter = adapter
+        })
+
+        // Observe the error message.
+        viewModel.error.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        })
     }
 
     private fun initViews(){
         btnSubmit.setOnClickListener {
-            val data = viewModel.movies
-            val dataValus = viewModel.movies.value
-
-            println("yes")
+            gv_movies.invalidateViews()
         }
-
-        rvMovies.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
-        rvMovies.adapter = this.movieAdapter
     }
 
-    class MovieAdapter(private val mainActivity: MainActivity, private val movies: List<Movie>) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
-        lateinit var context: Context
+    class MovieAdapter(private var context: Context, var movieList: List<Movie>) : BaseAdapter() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            context = parent.context
-            val view= LayoutInflater.from(context).inflate(R.layout.activity_movie, parent, false)
+        override fun getCount(): Int {
+            return movieList.size
+        }
 
-            view.setOnClickListener {
-                val intent = Intent(mainActivity, MovieDetailsActivity::class.java)
+        override fun getItem(position: Int): Any {
+            return movieList[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        @SuppressLint("ViewHolder", "InflateParams")
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val movie = this.movieList[position]
+
+            var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var movieView = inflator.inflate(R.layout.movie, null)
+
+            movieView.tvNumber.text = position.toString()
+            movieView.ivMovie.setImageResource(R.mipmap.ic_launcher)
+
+            movieView.setOnClickListener {
+                val intent = Intent(context, MovieDetailsActivity::class.java)
                 context.startActivity(intent)
             }
 
-            return ViewHolder(view)
-        }
-
-        override fun getItemCount(): Int {
-            return movies.size
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int)
-        {
-            holder.bind(movies[position])
-        }
-
-        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-            fun bind(movie : Movie) {
-                itemView.tvNumber.text = "0"
-                itemView.ivMovie.setImageResource(R.mipmap.ic_launcher)
-            }
+            return movieView
         }
     }
 }
